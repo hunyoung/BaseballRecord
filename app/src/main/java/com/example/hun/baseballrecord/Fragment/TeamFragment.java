ff5d061b8fa55f083fbbc9a75d6781d5cc097e70
hunyoung <chunghyhj@tvstorm.com>
2018-12-01 17:49:22 +0900

    add team record recyclerview

--- app/src/main/java/com/example/hun/baseballrecord/Fragment/TeamFragment.java
+++ app/src/main/java/com/example/hun/baseballrecord/Fragment/TeamFragment.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.hun.baseballrecord.Adapter.TeamFrgmentRecyclerAdapter;
 import com.example.hun.baseballrecord.Model.TeamFragmentRecyclerModel;
@@ -33,7 +34,10 @@ public class TeamFragment extends Fragment {
     private View mRootView;
     private List<TeamFragmentRecyclerModel> dataList = null;
     private TeamFrgmentRecyclerAdapter mTeamFrgmentRecyclerAdapter = null;
+    private List<String> htmlList = new ArrayList<>();
     private RecyclerView teamRecyclerView;
+    private TextView mCurDate;
+    private String dateString;
 
 
     public TeamFragment() {
@@ -63,20 +67,20 @@ public class TeamFragment extends Fragment {
         Log.d(TAG, "init()");
 
         teamRecyclerView = mRootView.findViewById(R.id.teamRecyclerView);
-
         dataList = new ArrayList<>();
+        mCurDate = mRootView.findViewById(R.id.date);
 
         addMainMenuDummy();
-        setRecyclerView();
-//        TeamFragment.JsoupAsyncTask jsoupAsyncTask = new TeamFragment.JsoupAsyncTask();
-//        jsoupAsyncTask.execute();
+//        setRecyclerView();
+        TeamFragment.JsoupAsyncTask jsoupAsyncTask = new TeamFragment.JsoupAsyncTask();
+        jsoupAsyncTask.execute();
 
     }
 
     private void addMainMenuDummy() {
         Log.d(TAG, "addMainMenuDummy");
-        dataList.add(new TeamFragmentRecyclerModel("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"));
-        dataList.add(new TeamFragmentRecyclerModel("10", "20", "30", "40", "50", "60", "70", "80", "90", "100", "110", "120"));
+        dataList.add(new TeamFragmentRecyclerModel("순위", "팀명", "경기", "승", "패", "무", "승률", "게임차", "최근10경기", "연속", "홈", "원정"));
+//        dataList.add(new TeamFragmentRecyclerModel("10", "20", "30", "40", "50", "60", "70", "80", "90", "100", "110", "120"));
 
     }
 
@@ -91,50 +95,53 @@ public class TeamFragment extends Fragment {
     }
 
 
-//
-//    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
-//
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//        }
-//
-//        @Override
-//        protected Void doInBackground(Void... params) {
-//            try {
-//                Document doc = Jsoup.connect(htmlURL).get();
-//
-//                //테스트1
-//                Elements titles = doc.select("table.table tr td");
-//                //Elements titles= doc.select("div.box-body tr td");    //한개 씩 뜯어서 나옴
-//                int i = 0;
-//                for (Element e : titles) {
-//                    htmlList.add(e.text());
-//                    //       dataList.add(new RecyclerModel(String.valueOf(++i), e.text()));
-//                }
-//
-//                String link = "http://www.statiz.co.kr/" + doc.select("table.table tr td a").attr("href");
-//                Log.d(TAG, "link ====>>> " + link);
-//
-//                for (int a = 0; a < 929 /*htmlList.size()*/; a++) {
-//                    if (a % 31 == 1) {
-//                        //                Log.d(TAG,"a ==== " + a + " 값 ==>  " + htmlList.get(a));
-//                        dataList.add(new RecyclerModel(String.valueOf(++i), htmlList.get(a), htmlList.get(a + 28)));
-//
-//                    }
-//                }
-//
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Void result) {
-//            setRecyclerView();
-//        }
-//    }
-//
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Document doc = Jsoup.connect(htmlURL).get();
+
+                //테스트1
+                Elements titles = doc.select("table.tData tr td");
+                Elements currentDate = doc.select("span.exp2");
+                Log.d(TAG, "e.text ==>>  " + currentDate.text());
+                dateString = currentDate.text();
+                int i = 0;
+                for (Element e : titles) {
+                    htmlList.add(e.text());
+//                    Log.d(TAG, "e.text ==>>  " + e.text());
+
+                }
+
+                for (int a = 0; a < 120 /*htmlList.size()*/; a++) {
+                    if (a % 12 == 0) {
+                //        Log.d(TAG,"a ==== " + a + " 값 ==>  " + htmlList.get(a));
+                        dataList.add(new TeamFragmentRecyclerModel(htmlList.get(a), htmlList.get(a+1), htmlList.get(a+2), htmlList.get(a+3),
+                                htmlList.get(a+4), htmlList.get(a+5), htmlList.get(a+6), htmlList.get(a+7), htmlList.get(a+8),
+                                htmlList.get(a+9), htmlList.get(a+10), htmlList.get(a+11)));
+
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            setRecyclerView();
+            mCurDate.setText(dateString);
+        }
+    }
+
 
 }
