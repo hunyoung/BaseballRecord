54c2c6318beabab3bc590cbd007a7266cf522665
hunyoung <chunghyhj@tvstorm.com>
2018-12-10 23:21:04 +0900

    edittext 입력후 버튼 선택시 키보드 사라지도록 수정

--- app/src/main/java/com/example/hun/baseballrecord/Activity/VideoActivity.java
+++ app/src/main/java/com/example/hun/baseballrecord/Activity/VideoActivity.java
@@ -77,6 +77,7 @@ public class VideoActivity extends YouTubeBaseActivity {
             @Override
             public void onClick(View v) {
                 searchTask = new searchTask().execute();
+                hideKeyboard();
             }
         });
 
